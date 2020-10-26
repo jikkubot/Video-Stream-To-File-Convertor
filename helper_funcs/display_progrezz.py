c9531c52a1f190779c6d6868c836008ffc1c43aa
jikkubot <66976170+jikkubot@users.noreply.github.com>
2020-10-27 02:18:37 +0400

    Update display_progrezz.py

--- helper_funcs/display_progrezz.py
+++ helper_funcs/display_progrezz.py
@@ -43,8 +43,8 @@ async def progrezz_for_pyrogram(
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress ="\n📥Percentage: {2}%\n\n[{0}{1}]\n\n".format(
-            ''.join(["f" for i in range(math.floor(percentage / 5))]),
-            ''.join(["v" for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["◼" for i in range(math.floor(percentage / 5))]),
+            ''.join(["◻" for i in range(20 - math.floor(percentage / 5))]),
           round(percentage, 2))
 
         tmp = progress + "\n📥 <b>Downloaded</b>: `{0}` \n\n 📁 <b>Total Size</b>: `{1}`\n\n🚀 <b>Speed</b>: `{2}/s`\n\n⏳ <b>Time Left</b>: `{3}`\n".format(
