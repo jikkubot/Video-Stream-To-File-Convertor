e87a00e9e5616a71208e6f65c79f95be1f0d053b
jikkubot <66976170+jikkubot@users.noreply.github.com>
2020-10-27 01:50:42 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## telegram file To Video Converter Bot With Permanent Thumbnail Support 
+---
+
+An Open Source Telegram Converter Bot Based On [Anydl](https://telegram.dog/spechide)
+
+### Special thanks
+
+* [@Spechide](https://telegram.dog/spechide)
+
+### Installation
+
+#### The Easy Way
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Our Telegram Channel
+
+* [Mai Bots](https://telegram.dog/Mai_bOTs)
+
+
+
+```
+start - Check if the Bot is Online!
+help - How to use this Bot?
+upgrade - Upgrade your status
+converttovideo - Convert to Streamable Video
+```
+
---
+++ translation.py
@@ -0,0 +1,31 @@
+class Translation(object):
+    START_TEXT = """<b>Hello there</b>,<b> I'm a Telegram File convert Bot!</b> \n<b>1.Send Me Any Telegram Document Or File</b> \n <b>2.Reply To That File With /converttovideo</b>\n <b>3. I'll Convert It Into Video With Permanent Thumbnail Support.</b>\n  
+Bot By : @mAi_BotS
+    """
+    ABS_TEXT = " Please don't be selfish."
+    BANNED_USER_TEXT = "you are banned"
+    UPGRADE_TEXT = "No upgrade plan Yet Contact. @No_OnE_Kn0wS_Me"
+    DOWNLOAD_START = "<b>trying to download</b>"
+    UPLOAD_START = "<b>trying to upload</b>"
+    RCHD_BOT_API_LIMIT = "<b>size greater than maximum allowed size (50MB). Neverthless, trying to upload.</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank You For Using Meh</b> \n <b>Join</b> : @MaI_BoTs <b>For Other Useful Bots</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease Join Here @MaI_BoTs if you find me useful. \nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    SAVED_CUSTOM_THUMB_NAIL = "<b>✅ Custom video thumbnail saved.</b> <b>This Img Is Permanent Until</b> /deletethumbnail"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>Document Downloaded Successfully.</b>"
+    CUSTOM_CAPTION_UL_FILE = ""
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    CURENT_PLAN_DETAILS = """Current plan details
+--------
+Telegram ID: <code>{}</code>
+Support: <a href='https://t.me/Mai_bOTs'>{}</a>
+Expires on: {}"""
+    HELP_USER = """I can :
+👉 <a href="https://telegram.dog/Mai_bOTs">Convert Any Documents/File Into Streamable Video With Permanent Thumbnail Support</a>
+--------
+
+"""
+
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
